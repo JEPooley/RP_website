2923b9214174ccbce55131e6080c72c2f708eefe
HappyGnome <ben.c.pooley@gmail.com>
2019-01-24 20:22:03 +0000

    Changed microdata for JSON-LD in a separate file

---
+++ LocalBusinessJSON.js
@@ -0,0 +1,27 @@
+/*
+Define structured data about the business
+*/
+{
+	"@context":"https://schema.org/",
+	"@type":"LocalBusiness",
+	"@id":"https://www.robertpooleymaths.co.uk",
+	"name":"Robert Pooley",
+	"description":"Maths Tutor in East Devon",
+	"address":{
+		"@type":"PostalAddress",
+		"streetAddress":"Daisymount House, Exeter Road",
+		"addressLocality":"Ottery St Mary", 
+		"addressRegion":"Devon",
+		"postalCode":"EX11 1LE",
+		"addressCountry":"GB"
+	},
+	"image":{
+		"@type":"ImageObject",
+		"contentUrl":"https://www.robertpooleymaths.co.uk/Images/at_desk.jpg",
+		"url":"https://www.robertpooleymaths.co.uk/Images/at_desk.jpg"
+	},
+	"telephone":"01404 822333",
+	"email":"robertpooleymaths@gmail.com",
+	"priceRange":"£25-£30 per hour",
+	"knowsAbout":["Mathematics","Maths","Physics"]
+}
